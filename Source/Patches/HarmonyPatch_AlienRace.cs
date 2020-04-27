e0ffbd110d83ae8a0822873c0a0b71f3a11f6541
Platinum <dominiclopez391@gmail.com>
2020-04-27 00:14:36 -0700

    no-har

--- Source/Patches/HarmonyPatch_AlienRace.cs
+++ Source/Patches/HarmonyPatch_AlienRace.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿/*
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -138,4 +139,4 @@ namespace Rimworld_Animations {
 			return false;
 		}
 	}
-}
+}*/
