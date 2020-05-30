218e4426c8f234998b984c70563544b6e9461f3c
Platinum <dominiclopez391@gmail.com>
2020-05-29 23:26:20 -0700

    changed drawtracker to move with offset

--- Source/Patches/HarmonyPatch_Pawn_DrawTracker.cs
+++ Source/Patches/HarmonyPatch_Pawn_DrawTracker.cs
@@ -13,8 +13,16 @@ namespace Rimworld_Animations {
     public static class HarmonyPatch_Pawn_DrawTracker {
         public static bool Prefix(ref Pawn ___pawn, ref Vector3 __result) {
 
-            if(___pawn.TryGetComp<CompBodyAnimator>() != null && ___pawn.TryGetComp<CompBodyAnimator>().isAnimating) {
+            CompBodyAnimator bodyAnim = ___pawn.TryGetComp<CompBodyAnimator>();
+
+            if (bodyAnim != null && bodyAnim.isAnimating) {
                 __result = ___pawn.TryGetComp<CompBodyAnimator>().anchor + ___pawn.TryGetComp<CompBodyAnimator>().deltaPos;
+
+                if (bodyAnim.CurrentAnimation?.actors[bodyAnim.ActorIndex]?.offsetsByDefName != null && bodyAnim.CurrentAnimation.actors[bodyAnim.ActorIndex].offsetsByDefName.ContainsKey(___pawn.def.defName)) {
+                    __result.x += bodyAnim.CurrentAnimation.actors[bodyAnim.ActorIndex].offsetsByDefName[___pawn.def.defName].x;
+                    __result.z += bodyAnim.CurrentAnimation.actors[bodyAnim.ActorIndex].offsetsByDefName[___pawn.def.defName].y;
+                }
+
                 return false;
             }
             return true;
